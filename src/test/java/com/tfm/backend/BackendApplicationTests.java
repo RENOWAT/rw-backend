f97f59ea7d0e8a9d478cfdf66cec38e076aa1520
Jaime <jaimechacons@hotmail.com>
2023-05-10 23:46:12 +0200

    Initial

---
+++ src/test/java/com/tfm/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
