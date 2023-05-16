3d52ec6d7fe9f1cee70843ceca7c5678bf4f2966
Jaime <jaimechacons@hotmail.com>
2023-05-16 20:37:40 +0200

    Added exception configuration and OpenAPI #RNW-11

---
+++ src/main/java/com/tfm/backend/services/exceptions/ConflictException.java
@@ -0,0 +1,9 @@
+package com.tfm.backend.services.exceptions;
+
+public class ConflictException extends RuntimeException {
+    private static final String DESCRIPTION = "Conflict Exception";
+
+    public ConflictException(String detail) {
+        super(DESCRIPTION + ". " + detail);
+    }
+}
